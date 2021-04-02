3612e7d37ba286cb553bd3451f3b76a320a81302
johnleles <johnleles.dev@gmail.com>
2021-04-01 21:11:10 -0300

    Segundo commit (Calculadora Simples) 01/04

--- src/pct/CalculadoraSimples.java
+++ src/pct/CalculadoraSimples.java
@@ -9,35 +9,69 @@ import java.util.Scanner;
 
 /**
  *
- * @author jwict
+ * @author johnleles
  */
 public class CalculadoraSimples {
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
-        //Declaração de variáveis
-        int n1, n2, total;
         
-        //Criação e instância do objeto de entrada
+        //Declaracao de variaveis
+        int n1, n2, adicao, subtracao, multiplicacao, opcao, escolha, divisao, resto;
+        
+        //Objeto de entrada
         Scanner entrada = new Scanner(System.in);
+        System.out.println("Calculadora");
         
-        //Apresentação
-        System.out.println("\n\t\t\t Calculadora Simples \n");
-        
-
-        System.out.println("Soma");
-        
-        //Entrada
-        System.out.print("Informe N1:");
+        System.out.println("Digite o primeiro número da operação:");
         n1 = entrada.nextInt();
-        
-        System.out.print("Informe N2:");
+        System.out.println("Digite o segundo número da operação:");
         n2 = entrada.nextInt();
         
-        //Processamento
         
-        total = n1 + n2;
+        //Apresentaçao
+        System.out.println("\n\t\t\t CALCULADORA \n");
         
-        //Saída
+        //Menu
+        System.out.println("Selecione o número correspondente da operação desejada ou tecle 5 para sair.");
+        System.out.println ("1 - Adição");
+        System.out.println ("2 - Subtração");   
+        System.out.println ("3 - Multiplicação");
+        System.out.println ("4 - Divisão");
+        System.out.println ("5 - Resto");
+        System.out.println ("6 - Sair");
+        //opcao = entrada.nextInt();
         
-        System.out.printf("\n\t%d + %d = %d\n", n1, n2, total);
+        
+        //Opção do usuário
+        escolha = entrada.nextInt();       
+        switch(escolha) {
+            
+            case 1: 
+                adicao = n1+n2;
+                System.out.println("O resoltado da soma é:"+ adicao);
+                break;
+            case 2: 
+                subtracao = n1-n2;
+                System.out.println("O resoltado da subtração é:"+ subtracao);
+                break;
+            case 3: 
+                multiplicacao = n1*n2;
+                System.out.println("O resoltado da multiplicação é:"+ multiplicacao);
+                break;
+            case 4: 
+                divisao = n1/n2;
+                System.out.println("O resoltado da divisão é:"+ divisao);
+                break;
+            case 5: 
+                resto = n1%n2;
+                System.out.println("O resoltado do resto é:"+ resto);
+                break; 
+            default:
+                System.out.println("Adeus!");       
+            }            
     }
-}
+               
+          
+
+        
+    }
